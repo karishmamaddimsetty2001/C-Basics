b80ed7f4132322ac05224b624ec1aa14cdf854e5
karishmamaddimsetty2001 <92290355+karishmamaddimsetty2001@users.noreply.github.com>
2021-10-22 21:51:12 +0530

    Add files via upload

---
+++ biodata of student.c
@@ -0,0 +1,17 @@
+//print student biodata
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char intial;
+	//intialisation
+	age =20;
+	cgpa =8.7;
+	intial ='m';
+	//output
+	printf("age of student is %d\n",age);
+	printf("cgpa of student is %f\n",cgpa);
+	printf("intial of student is %c\n",intial);
+}
