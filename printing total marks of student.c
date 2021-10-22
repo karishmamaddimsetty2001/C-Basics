43d41834e10d4388bb1e4d78501b84e496d68f2a
karishmamaddimsetty2001 <92290355+karishmamaddimsetty2001@users.noreply.github.com>
2021-10-22 21:50:37 +0530

    Add files via upload

---
+++ printing total marks of student.c
@@ -0,0 +1,17 @@
+//printing total marks of a student in diff subject 
+#include<stdio.h>
+void main()
+{
+	
+   
+    //variable declaration
+	int phy,chem,math,eng,tm;
+	//intialisation
+	phy= 60;
+	chem= 70;
+	math= 65;
+	eng= 90;
+	tm = phy+chem+math+eng;
+	//output
+	printf("total marks of the students is%d",tm);	
+}
